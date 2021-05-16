ce65117a58aa95e15c71f008f0274ead876837d7
Andreas <35858164+a-doering@users.noreply.github.com>
2021-05-17 01:51:41 +0200

    Add documentation

--- src/kinematics/robot_arm_update.py
+++ src/kinematics/robot_arm_update.py
@@ -7,7 +7,7 @@ import time
 
 
 class RobotArm2d():
-    """2D PRRR robot arm from ardizzone et al."""
+    """2D PRRR robot arm from ardizzone et al. (prismatic, rotational, rotational, rotational"""
     def __init__(self, lengths=[0.5, 0.5, 1], sigmas=[0.25, 0.5, 0.5, 0.5]):
         self.sigmas = np.array(sigmas)
         self.lengths = np.array(lengths)
@@ -16,7 +16,12 @@ class RobotArm2d():
         cmap = cm.tab20c
         self.colors = [[cmap(4*c_index), cmap(4*c_index+1), cmap(4*c_index+2)] for c_index in range(5)][-1]
 
-    def forward(self, parameters):
+    def forward(self, parameters: np.array) -> np.array:
+        """Forward kinematics of given joint configuration
+
+        :param parameters: Joint parameters: distance, theta2, theta3, theta4
+        :return: End effector position in 2d as np.array
+        """
         t1, t2, t3, t4 = parameters
         l1, l2, l3 = self.lengths
         # TODO: investigate why ardizzone took different angle directions (signs in sin and cos)
@@ -25,10 +30,20 @@ class RobotArm2d():
         return np.array([x, y])
 
     def sample_priors(self):
+        """Normal distributed values of the joint parameters"""
         return np.random.randn(4) * self.sigmas
 
-    def inverse(self, pos_tip, guess, epsilon=5e-2, max_steps=3000, lr=0.2):
-        pos_tip = np.array(pos_tip)
+    def inverse(self, tcp_pos: np.array, guess: np.array, epsilon=5e-2, max_steps=3000, lr=0.2) -> np.array:
+        """Inverse kinematics with gradient descent
+
+        :param tcp_pos: Position of the tool center point of the end effector as list or np.array of size (2)
+        :param guess: Inital guess for the joint parameters, list or np.array of size (4)
+        :param epsilon: Tolerance for prediction compared to ground truth, float
+        :param max_steps: Maximum number of steps for gradient descent
+        :param lr: Learn rate, float
+        :return: Joint parameters as np.array of size (4)
+        """
+        tcp_pos = np.array(tcp_pos)
         guess = np.array(guess)
         l1, l2, l3 = self.lengths
         # Gradient descent
@@ -40,7 +55,7 @@ class RobotArm2d():
             J = np.array([[0, - l1 * np.sin(t2) - l2 * np.sin(t2 + t3) - l3 * np.sin(t2 + t3 + t4), - l2 * np.sin(t2 + t3) - l3 * np.sin(t2 + t3 + t4), - l3 * np.sin(t2 + t3 + t4)],
                          [1, l1 * np.cos(t2) + l2 * np.cos(t2 + t3) + l3 * np.cos(t2 + t3 + t4), l2 * np.cos(t2 + t3) + l3 * np.cos(t2 + t3 + t4),  l3 * np.cos(t2 + t3 + t4)]])
             forward = self.forward(guess)
-            error = pos_tip - forward
+            error = tcp_pos - forward
             guess = guess + lr * np.dot(J.T, error)
             if (np.linalg.norm(error) < epsilon):
                 break
@@ -48,11 +63,17 @@ class RobotArm2d():
         return guess
 
     def viz_forward(self, tcp_array: np.array, priors=None) -> None:
+        """Visualization of forward kinematics positions
+
+        :param tcp_array: Tool center point np.array of size (n, 2)
+        :param priors: Joint priors corresponding to tcp_array of size (n, 4)
+        """
         fig = self.init_plot()
         if priors is not None:
             # TODO: implement plotting of arm joints
             pass
         else:
+            # Plotting of tcp positions
             plt.scatter(tcp_array[:, 0], tcp_array[:, 1], s=5)
         plt.xlim(*self.rangex)
         plt.ylim(*self.rangey)
@@ -60,11 +81,16 @@ class RobotArm2d():
         plt.title(f"Forward Kinematics with {tcp_array.shape[0]} samples")
         plt.show()
 
-    def init_plot(self) -> None:
+    def init_plot(self) -> plt.figure:
+        """Initialize matplotlib figure"""
         return plt.figure(figsize=(8, 8))
 
     def viz_inverse(self, tcp_pos: np.array, guesses_array: np.array) -> None:
+        """Visualization of inverse kinematic configurations for one tcp position with all arms
 
+        :param tcp_pos: Tool center point position, np.array of size (2)
+        :param guesses_array: Joint configurations that lead to tcp_pos, np.array of size (n, 4)
+        """
         l1, l2, l3 = self.lengths
         starting_pos = np.zeros((guesses_array.shape[0], 2))
         starting_pos[:, 1] = guesses_array[:, 0]
@@ -99,7 +125,7 @@ class RobotArm2d():
         plt.show()
 
     def save(self):
-        pass
+        raise NotImplementedError("TODO")
 
 
 if __name__ == "__main__":
