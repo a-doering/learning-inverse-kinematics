554b41a80abcf37cb466aff534412ad362ea66f8
Arman Mielke <arman.mielke@yahoo.de>
2021-05-30 19:07:32 +0200

    INN: Reduce model parameters

--- src/inn/model.py
+++ src/inn/model.py
@@ -3,7 +3,7 @@ from FrEIA.modules import RNVPCouplingBlock
 from torch import nn
 
 
-def create_inn(n_dim: int = 2) -> SequenceINN:
+def create_inn(n_dim: int) -> SequenceINN:
     # simple chain of RealNVP coupling blocks
     inn = SequenceINN(n_dim)
     for k in range(3):
@@ -15,7 +15,7 @@ def create_inn(n_dim: int = 2) -> SequenceINN:
 def _subnet_fc(dims_in, dims_out) -> nn.Sequential:
     """Creates a subnet for use inside an affine coupling block."""
     return nn.Sequential(
-        nn.Linear(dims_in, 256),
+        nn.Linear(dims_in, 128),
         nn.ReLU(),
-        nn.Linear(256, dims_out),
+        nn.Linear(128, dims_out),
     )
