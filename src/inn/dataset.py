b0554c3a0567881ff01a109b0b2f26ef92218ec9
Arman Mielke <arman.mielke@yahoo.de>
2021-05-30 17:59:53 +0200

    Refactor INN training

--- src/inn/dataset.py
+++ src/inn/dataset.py
@@ -1,13 +1,16 @@
 import pickle
+from typing import Tuple
 
 import torch
 from torch.utils.data import TensorDataset
 
 
-def load_dataset(path: str = "data/forward.pickle") -> TensorDataset:
+def load_dataset(path: str = "data/forward.pickle") -> Tuple[TensorDataset, int, int]:
     """
     :param path: Path to the file containing the pickled training data
-    :return: A dataset that returns tuples of (priors, positions), each of size (batch_size, 2) and dtype=torch.float
+    :return: 1. A dataset that returns tuples of (priors, positions), each of size (batch_size, 2) and dtype=torch.float
+             2. Dimensionality of the priors vectors
+             3. Dimensionality of the position vectors
     """
     with open(path, "rb") as file:
         data = pickle.load(file)
@@ -15,4 +18,4 @@ def load_dataset(path: str = "data/forward.pickle") -> TensorDataset:
     priors = torch.tensor(data["priors"], dtype=torch.float)
     positions = torch.tensor(data["positions"], dtype=torch.float)
 
-    return TensorDataset(priors, positions)
+    return TensorDataset(priors, positions), priors.size(1), positions.size(1)
--- src/inn/train.py
+++ src/inn/train.py
@@ -2,6 +2,7 @@ import csv
 
 import numpy as np
 import torch
+from torch import nn
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader, random_split
@@ -11,14 +12,6 @@ from model import create_inn
 import mmd
 
 
-# TODO un-hard-code
-# priors dim
-X_DIM = 4
-# position dim
-Y_DIM = 2
-# X_DIM == Y_DIM + Z_DIM, therefore Z_DIM = X_DIM - Y_DIM
-Z_DIM = X_DIM - Y_DIM
-
 # factors for the loss terms
 FORWARD_FIT_FACTOR = 1
 FORWARD_MMD_FACTOR = 50
@@ -26,29 +19,29 @@ BACKWARD_MMD_FACTOR = 500
 RECONSTRUCTION_FACTOR = 1
 
 
-def loss_forward_mmd(out, y):
+def loss_forward_mmd(positions_pred: torch.tensor, positions: torch.tensor, position_dim: int, z_dim: int):
     # Shorten output, and remove gradients wrt y, for latent loss
-    output_block_grad = torch.cat((out[:, :Z_DIM], out[:, -Y_DIM:].data), dim=1)
-    y_short = torch.cat((y[:, :Z_DIM], y[:, -Y_DIM:]), dim=1)
+    output_block_grad = torch.cat((positions_pred[:, :z_dim], positions_pred[:, -position_dim:].data), dim=1)
+    y_short = torch.cat((positions[:, :z_dim], positions[:, -position_dim:]), dim=1)
 
-    l_forward_fit = FORWARD_FIT_FACTOR * mmd.l2_fit(out[:, Z_DIM:], y[:, Z_DIM:])
+    l_forward_fit = FORWARD_FIT_FACTOR * mmd.l2_fit(positions_pred[:, z_dim:], positions[:, z_dim:])
     l_forward_mmd = FORWARD_MMD_FACTOR * torch.mean(mmd.forward_mmd(output_block_grad, y_short))
 
     return l_forward_fit, l_forward_mmd
 
 
-def loss_backward_mmd(x, y, inn):
+def loss_backward_mmd(priors: torch.tensor, positions: torch.tensor, inn: nn.Module):
     # TODO use jac=False ?
-    x_samples, _ = inn(y, rev=True)
-    MMD = mmd.backward_mmd(x, x_samples)
-    return BACKWARD_MMD_FACTOR * torch.mean(MMD)
+    priors_pred, _ = inn(positions, rev=True)
+    backward_mmd = mmd.backward_mmd(priors, priors_pred)
+    return BACKWARD_MMD_FACTOR * torch.mean(backward_mmd)
 
 
-def loss_reconstruction(out_y, y, x, inn):
-    cat_inputs = [out_y[:, :Z_DIM], out_y[:, -Y_DIM:]]
+def loss_reconstruction(positions_pred: torch.tensor, priors: torch.tensor, inn: nn.Module, position_dim: int, z_dim: int):
+    cat_inputs = [positions_pred[:, :z_dim], positions_pred[:, -position_dim:]]
     # TODO use jac=False ?
     x_reconstructed, _ = inn(torch.cat(cat_inputs, 1), rev=True)
-    return RECONSTRUCTION_FACTOR * mmd.l2_fit(x_reconstructed, x)
+    return RECONSTRUCTION_FACTOR * mmd.l2_fit(x_reconstructed, priors)
 
 
 def train(
@@ -59,16 +52,17 @@ def train(
     val_set_portion: float = 0.1,  # portion of the dataset that will be used for validation
     log_file: str = "losses.csv",
 ):
-    inn = create_inn(X_DIM)
-    optimizer = Adam(inn.parameters(), lr=lr)
-    lr_scheduler = ReduceLROnPlateau(optimizer, patience=lr_scheduler_patience, verbose=True)
-
     # prepare dataset
-    dataset = load_dataset()
+    dataset, priors_dim, position_dim = load_dataset()
+    z_dim = priors_dim - position_dim
     val_set_size = int(len(dataset) * val_set_portion)
     train_dataset, val_dataset = random_split(dataset, [len(dataset) - val_set_size, val_set_size])
     train_loader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=2, drop_last=True)
     val_loader = DataLoader(val_dataset, batch_size, shuffle=True, num_workers=2, drop_last=True)
+
+    inn = create_inn(priors_dim)
+    optimizer = Adam(inn.parameters(), lr=lr)
+    lr_scheduler = ReduceLROnPlateau(optimizer, patience=lr_scheduler_patience, verbose=True)
 
     for epoch in range(epochs):
 
@@ -79,16 +73,16 @@ def train(
         inn.train()
 
         for priors, positions in train_loader:
-            noise_batch = torch.randn(batch_size, Z_DIM)
+            noise_batch = torch.randn(batch_size, z_dim)
             positions = torch.cat((noise_batch, positions), dim=1)
 
             # TODO use jac=False ?
             positions_pred, _ = inn(priors)
 
             batch_losses = []
-            batch_losses.extend(loss_forward_mmd(positions_pred, positions))
+            batch_losses.extend(loss_forward_mmd(positions_pred, positions, position_dim, z_dim))
             batch_losses.append(loss_backward_mmd(priors, positions, inn))
-            batch_losses.append(loss_reconstruction(positions_pred.data, positions, priors, inn))
+            batch_losses.append(loss_reconstruction(positions_pred.data, priors, inn, position_dim, z_dim))
 
             train_loss_history.append([batch_loss.item() for batch_loss in batch_losses])
 
@@ -108,21 +102,21 @@ def train(
 
         with torch.no_grad():
             for priors, positions in val_loader:
-                noise_batch = torch.randn(batch_size, Z_DIM)
+                noise_batch = torch.randn(batch_size, z_dim)
                 positions = torch.cat((noise_batch, positions), dim=1)
 
                 # TODO use jac=False ?
                 positions_pred, _ = inn(priors)
 
                 batch_losses = []
-                batch_losses.extend(loss_forward_mmd(positions_pred, positions))
+                batch_losses.extend(loss_forward_mmd(positions_pred, positions, position_dim, z_dim))
                 batch_losses.append(loss_backward_mmd(priors, positions, inn))
-                batch_losses.append(loss_reconstruction(positions_pred.data, positions, priors, inn))
+                batch_losses.append(loss_reconstruction(positions_pred.data, priors, inn, position_dim, z_dim))
 
                 val_loss_history.append([batch_loss.item() for batch_loss in batch_losses])
 
-            val_loss_mean = np.mean(val_loss_history, axis=0)
-            print(f"[Epoch {epoch}] Val loss:   {np.mean(val_loss_mean)}, {val_loss_mean}")
+        val_loss_mean = np.mean(val_loss_history, axis=0)
+        print(f"[Epoch {epoch}] Val loss:   {np.mean(val_loss_mean)}, {val_loss_mean}")
 
         # log losses
         with open(log_file, 'a+', newline='') as file:
