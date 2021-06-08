94930a578ed5e62499702efb42e8883bdaa1a671
Arman Mielke <arman.mielke@yahoo.de>
2021-06-08 02:51:23 +0200

    Calculate backward MMD using the test set

--- src/evaluate_with_mmd.py
+++ src/evaluate_with_mmd.py
@@ -7,6 +7,7 @@ from inn.mmd import forward_mmd
 from inn.model import create_inn
 
 
+BATCH_SIZE = 10000
 PRIORS_DIM = 4
 Z_DIM = 2
 
@@ -27,18 +28,37 @@ def evaluate():
 
     inn = load_model()
 
-    train_priors = torch.tensor(train_data["priors"], dtype=torch.float)[:10000, :]
-    train_positions = torch.tensor(train_data["positions"], dtype=torch.float)[:10000, :]
+    train_priors = torch.tensor(train_data["priors"], dtype=torch.float)[:BATCH_SIZE, :]
+    train_positions = torch.tensor(train_data["positions"], dtype=torch.float)[:BATCH_SIZE, :]
+    test_priors = torch.tensor(test_data["priors"], dtype=torch.float)[:BATCH_SIZE, :]
+    test_positions = torch.tensor(test_data["positions"], dtype=torch.float)[:BATCH_SIZE, :]
+
+    # Forward
+    ##########
+
     with torch.no_grad():
         predicted_positions, _ = inn(train_priors)
         predicted_positions = predicted_positions[:, Z_DIM:]
 
-    test_positions = torch.tensor(test_data["positions"], dtype=torch.float)[:10000, :]
-
+    print("Forward:")
     print("MMD between predicted positions and test set positions: ", end="")
     print(torch.mean(forward_mmd(predicted_positions, test_positions)).item())
-    print("MMD between train set positions and test seet positions: ", end="")
+    print("MMD between train set positions and test set positions: ", end="")
     print(torch.mean(forward_mmd(train_positions, test_positions)).item())
+
+    # Backward
+    ###########
+
+    noise_batch = torch.randn(BATCH_SIZE, Z_DIM)
+    train_positions_with_noise = torch.cat((noise_batch, train_positions), dim=1)
+    with torch.no_grad():
+        predicted_priors, _ = inn(train_positions_with_noise, rev=True)
+
+    print("Backward:")
+    print("MMD between predicted priors and test set priors: ", end="")
+    print(torch.mean(forward_mmd(predicted_priors, test_priors)).item())
+    print("MMD between train set priors and test set priors: ", end="")
+    print(torch.mean(forward_mmd(train_priors, test_priors)).item())
 
 
 if __name__ == "__main__":
