7c1d7db1523ad8a7063db15319dc889d9e4dee50
Arman Mielke <arman.mielke@yahoo.de>
2021-06-08 16:24:00 +0200

    Evaluate using only the test set

--- src/evaluate_with_mmd.py
+++ src/evaluate_with_mmd.py
@@ -21,44 +21,44 @@ def load_model(checkpoint_path: str = "very-long-log/125_checkpoint.pt") -> Sequ
 
 
 def evaluate():
-    with open("data/train.pickle", "rb") as train_file:
-        train_data = pickle.load(train_file)
     with open("data/test.pickle", "rb") as test_file:
         test_data = pickle.load(test_file)
 
     inn = load_model()
 
-    train_priors = torch.tensor(train_data["priors"], dtype=torch.float)[:BATCH_SIZE, :]
-    train_positions = torch.tensor(train_data["positions"], dtype=torch.float)[:BATCH_SIZE, :]
-    test_priors = torch.tensor(test_data["priors"], dtype=torch.float)[:BATCH_SIZE, :]
-    test_positions = torch.tensor(test_data["positions"], dtype=torch.float)[:BATCH_SIZE, :]
+    priors = torch.tensor(test_data["priors"], dtype=torch.float)
+    positions = torch.tensor(test_data["positions"], dtype=torch.float)
+    priors_a = priors[:BATCH_SIZE, :]
+    positions_a = positions[:BATCH_SIZE, :]
+    priors_b = priors[BATCH_SIZE:2*BATCH_SIZE, :]
+    positions_b = positions[BATCH_SIZE:2*BATCH_SIZE, :]
 
     # Forward
     ##########
 
     with torch.no_grad():
-        predicted_positions, _ = inn(train_priors)
+        predicted_positions, _ = inn(priors_a)
         predicted_positions = predicted_positions[:, Z_DIM:]
 
     print("Forward:")
-    print("MMD between predicted positions and test set positions: ", end="")
-    print(torch.mean(forward_mmd(predicted_positions, test_positions)).item())
-    print("MMD between train set positions and test set positions: ", end="")
-    print(torch.mean(forward_mmd(train_positions, test_positions)).item())
+    print("MMD between predicted positions and positions from the ground truth distribution: ", end="")
+    print(torch.mean(forward_mmd(predicted_positions, positions_b)).item())
+    print("MMD between two samples of positions from the ground truth distribution: ", end="")
+    print(torch.mean(forward_mmd(positions_a, positions_b)).item())
 
     # Backward
     ###########
 
     noise_batch = torch.randn(BATCH_SIZE, Z_DIM)
-    train_positions_with_noise = torch.cat((noise_batch, train_positions), dim=1)
+    positions_a_with_noise = torch.cat((noise_batch, positions_a), dim=1)
     with torch.no_grad():
-        predicted_priors, _ = inn(train_positions_with_noise, rev=True)
+        predicted_priors, _ = inn(positions_a_with_noise, rev=True)
 
     print("Backward:")
-    print("MMD between predicted priors and test set priors: ", end="")
-    print(torch.mean(forward_mmd(predicted_priors, test_priors)).item())
-    print("MMD between train set priors and test set priors: ", end="")
-    print(torch.mean(forward_mmd(train_priors, test_priors)).item())
+    print("MMD between predicted priors and priors from the ground truth distribution: ", end="")
+    print(torch.mean(forward_mmd(predicted_priors, priors_b)).item())
+    print("MMD between two samples of priors from the ground truth distribution: ", end="")
+    print(torch.mean(forward_mmd(priors_a, priors_b)).item())
 
 
 if __name__ == "__main__":
