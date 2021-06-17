d9b3c205adfc82a9457708250a73eebe7b9c1f67
Arman Mielke <arman.mielke@yahoo.de>
2021-06-17 23:53:37 +0200

    Plot thetas and null space

--- src/plot_distributions.py
+++ src/plot_distributions.py
@@ -1,6 +1,8 @@
 import pickle
 from FrEIA.framework import SequenceINN
 from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 import torch
 import torch.multiprocessing
 from torch.utils.data import DataLoader
@@ -58,5 +60,85 @@ def plot_ground_truth_position_distribution():
     plot_positions(test_data["positions"])
 
 
+def plot_thetas(batch_size: int = 128):
+    # Ground truth thetas
+    ######################
+
+    with open("data/test.pickle", "rb") as file:
+        data = pickle.load(file)
+    ground_truth_thetas = data["priors"]
+
+    scaler = StandardScaler()
+    ground_truth_thetas_scaled = scaler.fit_transform(ground_truth_thetas)
+    pca = PCA(n_components=2)
+    ground_truth_thetas_2d = pca.fit_transform(ground_truth_thetas_scaled)
+
+    plt.figure(figsize=(8, 8))
+    plt.xlim(-4.5, 4.5)
+    plt.ylim(-4.5, 4.5)
+    plt.scatter(ground_truth_thetas_2d[:, 0], ground_truth_thetas_2d[:, 1], s=5, alpha=0.02)
+    plt.draw()
+
+    # Predicted thetas
+    ###################
+
+    test_dataset, thetas_dim, position_dim = load_dataset("data/test.pickle")
+    test_loader = DataLoader(test_dataset, batch_size, num_workers=2)
+
+    inn = load_model(ground_truth_thetas.shape[1])
+
+    def apply_inn(positions_batch):
+        noise_batch = torch.randn(positions_batch.size(0), 2)
+        positions_with_noise = torch.cat((noise_batch, positions_batch), dim=1)
+        thetas, _ = inn(positions_with_noise, rev=True)
+        return thetas
+
+    # required for the list comprehension to work
+    torch.multiprocessing.set_sharing_strategy('file_system')
+    with torch.no_grad():
+        thetas_pred = [apply_inn(positions_batch) for _, positions_batch in test_loader]
+    thetas_pred = torch.cat(thetas_pred, dim=0)
+
+    thetas_pred_scaled = scaler.transform(thetas_pred)
+    thetas_pred_2d = pca.transform(thetas_pred_scaled)
+
+    plt.figure(figsize=(8, 8))
+    plt.xlim(-4.5, 4.5)
+    plt.ylim(-4.5, 4.5)
+    plt.scatter(thetas_pred_2d[:, 0], thetas_pred_2d[:, 1], s=5, alpha=0.02)
+    plt.show()
+
+
+def plot_ground_truth_null_space():
+    # there should be only one position with many priors
+    with open("data/inverse1.pickle", "rb") as file:
+        data = pickle.load(file)
+    ground_truth_thetas = data["posteriors"]
+    positions = torch.tensor(data["positions"], dtype=torch.float)
+    positions = positions.repeat(ground_truth_thetas.shape[0], 1)
+
+    scaler = StandardScaler()
+    ground_truth_thetas_scaled = scaler.fit_transform(ground_truth_thetas)
+    pca = PCA(n_components=2)
+    ground_truth_thetas_2d = pca.fit_transform(ground_truth_thetas_scaled)
+
+    inn = load_model(ground_truth_thetas.shape[1])
+    noise_batch = torch.randn(positions.shape[0], 2)
+    positions_with_noise = torch.cat((noise_batch, positions), dim=1)
+
+    with torch.no_grad():
+        thetas_pred, _ = inn(positions_with_noise, rev=True)
+    thetas_pred_scaled = scaler.transform(thetas_pred)
+    thetas_pred_2d = pca.transform(thetas_pred_scaled)
+
+    plt.figure(figsize=(8, 8))
+    plt.scatter(ground_truth_thetas_2d[:, 0], ground_truth_thetas_2d[:, 1], s=5, alpha=0.2)
+    plt.draw()
+
+    plt.figure(figsize=(8, 8))
+    plt.scatter(thetas_pred_2d[:, 0], thetas_pred_2d[:, 1], s=5, alpha=0.2)
+    plt.show()
+
+
 if __name__ == "__main__":
-    plot_predicted_position_distribution()
+    plot_thetas()
