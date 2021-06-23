d203de949cb1ffafe396d4f10e7c4852248569e5
Andreas <35858164+a-doering@users.noreply.github.com>
2021-06-23 11:30:53 +0000

    Extend robotarm to more rotational joints

--- src/kinematics/robot_arm_2d_torch.py
+++ src/kinematics/robot_arm_2d_torch.py
@@ -1,14 +1,14 @@
 import os
 import pickle
 import matplotlib.pyplot as plt
-from matplotlib import cm
 import torch
 from torch.functional import Tensor
 import time
 
-
 class RobotArm2d():
-    """2D PRRR robot arm from ardizzone et al. (prismatic, rotational, rotational, rotational"""
+    """2D PRRR robot arm from ardizzone et al. (prismatic, rotational, rotational, rotational)
+    Arm can have more or less rotational joints.
+    """
     def __init__(self, lengths: list = [0.5, 0.5, 1], sigmas: list = [0.25, 0.5, 0.5, 0.5]):
         cuda = True if torch.cuda.is_available() else False
         Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
@@ -17,11 +17,12 @@ class RobotArm2d():
         else:
             self.device = "cpu"
         self.sigmas = Tensor(sigmas)
+        self.num_joints = self.sigmas.shape[0]
         self.lengths = Tensor(lengths)
         self.rangex = (-0.5, 2.5)  # (-0.35, 2.25)
         self.rangey = (-1.5, 1.5)  # (-1.3, 1.3)
-        cmap = cm.tab20c
-        self.colors = [[cmap(4*c_index), cmap(4*c_index+1), cmap(4*c_index+2)] for c_index in range(5)][-1]
+        cmap = plt.cm.tab20c
+        self.colors = [[cmap(4*c_index + i) for i in range(self.lengths.shape[0])] for c_index in range(5)][-1]
         self.out_dir = "data"
         self.viz_dir = "visualizations"
         if not os.path.isdir(self.out_dir):
@@ -31,14 +32,14 @@ class RobotArm2d():
 
     def sample_priors(self, batch_size: int = 1) -> torch.FloatTensor:
         """Normal distributed values of the joint parameters"""
-        return torch.randn(batch_size, 4, device=self.device) * self.sigmas
+        return torch.randn(batch_size, self.num_joints, device=self.device) * self.sigmas
 
-    def advance_joint(self, current_pos: torch.FloatTensor, length: float, angle: float) -> (torch.FloatTensor, torch.FloatTensor):
+    def advance_joint(self, current_pos: torch.FloatTensor, length: float, angle: torch.FloatTensor) -> (torch.FloatTensor, torch.FloatTensor):
         """Calculate position of next joint
 
         :param current_pos: Current position at joint in 2d, size: (n, 2)
         :param length: Length of the arm from current to next position, float
-        :param angle: Angle around current joint, float
+        :param angle: Angle around current joint, (n)
         :return current_pos: Current position at joint in 2d, size: (n, 2)
         :return next_pos: Next position at end of arm in 2d, size: (n, 2)
         """
@@ -53,14 +54,15 @@ class RobotArm2d():
     def forward(self, thetas: torch.Tensor) -> torch.Tensor:
         """Forward kinematics of given joint configurations
 
-        :param thetas: Joint parameters: (n, 4)
+        :param thetas: Joint parameters: (n, self.num_joints)
         :return: End effector position in 2d: (n, 2)
         """
-        p0 = torch.stack([torch.zeros((thetas.shape[0]), device=thetas.device), thetas[:, 0]], axis=1)
-        _, p1 = self.advance_joint(p0, self.lengths[0], thetas[:, 1])
-        _, p2 = self.advance_joint(p1, self.lengths[1], thetas[:, 1] + thetas[:, 2])
-        _, p3 = self.advance_joint(p2, self.lengths[2], thetas[:, 1] + thetas[:, 2] + thetas[:, 3])
-        return p3
+        angle = torch.zeros_like(thetas[:, 1], device=self.device)
+        p_next = torch.stack([torch.zeros((thetas.shape[0]), device=thetas.device), thetas[:, 0]], axis=1)
+        for joint in range(self.num_joints -1):
+            angle += thetas[:, joint + 1]
+            _, p_next = self.advance_joint(p_next, self.lengths[joint], angle)
+        return p_next
 
     def distance_euclidean(self, pos_target: torch.FloatTensor, pos: torch.FloatTensor) -> float:
         """Calculate the pairwise distance between each position and its 
@@ -128,44 +130,38 @@ class RobotArm2d():
         plt.ylim(*self.rangey)
         plt.axvline(x=0, ls=':', c='gray', linewidth=.5)
         plt.title(f"Forward Kinematics with {pos.shape[0]} samples")
+        #TODO: implement saving routine
+        fig.savefig(os.path.join(self.viz_dir, "0000_forward") + ".png")
         plt.show()
 
     def viz_inverse(self, pos: torch.FloatTensor, thetas: torch.FloatTensor, save: bool = True, show: bool = False, fig_name: str = "fig", viz_format: tuple = (".png", ".svg")) -> None:
         """Visualization of inverse kinematic configurations for end effector position
 
         :param pos: End effector position, size (n, 2), use n=1 to get informative plots
-        :param thetas: Joint parameters, size (n, 4)
+        :param thetas: Joint parameters, size (n, self.num_joints)
         :param save: Bool, True if plot should be saved
         :param show: Bool, True if plot should be displayed
         :param fig_name: Name of the figure without ending or directory, e.g. "fig1"
         :param viz_format: Formats in which the plot should be saved, e.g. (".png", ".svg") or ("png",)
         """
 
-        # Calculate positions of each joint
-        p0 = torch.stack([torch.zeros((thetas.shape[0]), device=thetas.device), thetas[:, 0]], axis=1)
-        _, p1 = self.advance_joint(p0, self.lengths[0], thetas[:, 1])
-        _, p2 = self.advance_joint(p1, self.lengths[1], thetas[:, 1] + thetas[:, 2])
-        _, p3 = self.advance_joint(p2, self.lengths[2], thetas[:, 1] + thetas[:, 2] + thetas[:, 3])
-
+        # Setup plot
         fig = self.init_plot()
+        opts = {'alpha': 0.05, 'scale': 1, 'angles': 'xy', 'scale_units': 'xy', 'headlength': 0, 'headaxislength': 0, 'linewidth': 1.0, 'rasterized': True}
+
+        angle = torch.zeros_like(thetas[:, 1])
+        p_next = torch.stack([torch.zeros((thetas.shape[0]), device=thetas.device), thetas[:, 0]], axis=1)
+
+        for joint in range(self.num_joints -1):
+            # Advance one joint
+            angle += thetas[:, joint + 1]
+            p_current, p_next = self.advance_joint(p_next, self.lengths[joint], angle)
+            # Plot arm between the two positions
+            plt.quiver(p_current[:, 0], p_current[:, 1], (p_next-p_current)[:, 0], (p_next-p_current)[:, 1], **{'color': self.colors[joint], **opts})
+
 
         # Calculate distance from target
-        distance = self.distance_euclidean(pos, p3)
-
-        # Detach from gpu
-        if self.device == "cuda":
-            print("In loop")
-            p0 = p0.cpu().numpy()
-            p1 = p1.cpu().numpy()
-            p2 = p2.cpu().numpy()
-            p3 = p3.cpu().numpy()
-            pos = pos.cpu().numpy()
-
-        # Plot arms
-        opts = {'alpha': 0.05, 'scale': 1, 'angles': 'xy', 'scale_units': 'xy', 'headlength': 0, 'headaxislength': 0, 'linewidth': 1.0, 'rasterized': True}
-        plt.quiver(p0[:, 0], p0[:, 1], (p1-p0)[:, 0], (p1-p0)[:, 1], **{'color': self.colors[0], **opts})
-        plt.quiver(p1[:, 0], p1[:, 1], (p2-p1)[:, 0], (p2-p1)[:, 1], **{'color': self.colors[1], **opts})
-        plt.quiver(p2[:, 0], p2[:, 1], (p3-p2)[:, 0], (p3-p2)[:, 1], **{'color': self.colors[2], **opts})
+        distance = self.distance_euclidean(pos, p_next)
 
         # Plot cross to mark end effector position
         l_cross = 0.6
@@ -189,37 +185,67 @@ class RobotArm2d():
         """Generate training data: for each prior the end effector position is
         Calculated and for each end effector position num_inverses will be sampled
 
-        :param thetas: Joint parameters, size (n, 4)
+        :param thetas: Joint parameters, size (n, self.num_joints)
         :param num_inverses: Amount of inverses to be created per end effector position
         """
         pos = self.forward(thetas)
         num_positions = thetas.shape[0]
-        guesses = torch.zeros((num_positions * num_inverses, 4))
+        guesses = torch.zeros((num_positions * num_inverses, self.num_joints))
         # TODO: finish after implementing inverse kinematics
         raise NotImplementedError
 
 
 if __name__ == "__main__":
-    arm = RobotArm2d()
-    num_forward = 10
-    num_inverse_each = 100
+    #TODO: move this to a test class
+
+    ## 5 DOF P RRR R joints
+    arm = RobotArm2d(lengths = [0.5, 0.5, 1, 1], sigmas = [0.25, 0.5, 0.5, 0.5, 0.5])
+    num_forward = 3
+    num_inverse_each = 10
     # Viz forward
-    # priors = arm.sample_priors(num_forward)
-    # pos = arm.forward(priors)
-    # arm.viz_forward(pos)
+    priors = arm.sample_priors(num_forward)
+    pos = arm.forward(priors)
+    print(pos)
+    arm.viz_forward(pos)
     # # Viz and test inverse
     # guesses = arm.inverse(pos, arm.sample_priors(num_inverse_each))
     # arm.viz_inverse(pos, guesses)
 
     # Test inverse
     start = time.time()
-    num_forward = 100
-    num_inverse_each = 1000
+    num_forward = 2
+    num_inverse_each = 100
     priors = arm.sample_priors(num_forward)
     pos = arm.forward(priors)
+    arm.viz_forward(pos)
     thetas_gen = arm.inverse(pos, num_inverse_each)
     print(thetas_gen.shape)
     time_taken = time.time() - start
     print(f"time: {time_taken}")
     #print(thetas_gen)
-    arm.viz_inverse(pos[0:1], thetas_gen[0:num_inverse_each])
+    arm.viz_inverse(pos[0:1], thetas_gen[0:num_inverse_each], fig_name="0000_inverse")
+
+    # ## 4 DOF
+    # arm = RobotArm2d()
+    # num_forward = 10
+    # num_inverse_each = 100
+    # # Viz forward
+    # # priors = arm.sample_priors(num_forward)
+    # # pos = arm.forward(priors)
+    # # arm.viz_forward(pos)
+    # # # Viz and test inverse
+    # # guesses = arm.inverse(pos, arm.sample_priors(num_inverse_each))
+    # # arm.viz_inverse(pos, guesses)
+
+    # # Test inverse
+    # start = time.time()
+    # num_forward = 100
+    # num_inverse_each = 1000
+    # priors = arm.sample_priors(num_forward)
+    # pos = arm.forward(priors)
+    # thetas_gen = arm.inverse(pos, num_inverse_each)
+    # print(thetas_gen.shape)
+    # time_taken = time.time() - start
+    # print(f"time: {time_taken}")
+    # #print(thetas_gen)
+    # arm.viz_inverse(pos[0:1], thetas_gen[0:num_inverse_each])
